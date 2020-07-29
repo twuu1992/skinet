d976b84ad8557df31b318cbfd2341f3a9e4755d6
Tony Wu <astroneer976@gmail.com>
2020-07-29 23:46:03 +1000

    Refactor the paging info and convert into component;Refactor the pager component;

---
+++ client-ui/src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss'],
+})
+export class PagerComponent implements OnInit {
+  @Input() totalCount: number;
+  @Input() pageSize: number;
+  @Output() pageChanged = new EventEmitter();
+  constructor() {}
+
+  ngOnInit() {}
+
+  onPagerChanged(event: any) {
+    this.pageChanged.emit(event);
+  }
+}
