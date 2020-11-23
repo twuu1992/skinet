152322e839ba5ecaf02b348dbc9a1451908b9259
Tony Wu <astroneer976@gmail.com>
2020-11-23 22:44:58 +1100

    Enable cache;Cache products, brands, types;

--- web-api/Controllers/ProductsController.cs
+++ web-api/Controllers/ProductsController.cs
@@ -34,6 +34,7 @@ namespace web_api.Controllers
 
         }
 
+        [Cached(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
             [FromQuery] ProductSpecParams productParams)
@@ -51,6 +52,7 @@ namespace web_api.Controllers
             return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, itemCount, data));
         }
 
+        [Cached(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -65,12 +67,14 @@ namespace web_api.Controllers
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
+        [Cached(600)]
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok(await _productBrandRepo.ListAllAsync());
         }
 
+        [Cached(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
